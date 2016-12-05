92abf91d9a577180722d657d95b3da76a3e83a2c
wierje <wier.je@gmail.com>
2016-12-05 08:58:15 -0600

    mvp finished

--- scripts.js
+++ scripts.js
@@ -1,48 +1,48 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+    // checking DOM loading
     console.log("DOM fully loaded and parsed");
-    document.getElementById('howTall').focus();
+    // putting focus in the initial input
+    document.getElementById("howTall").focus();
 });
-console.log('Tree');
-
-var e = document.getElementById('button').addEventListener('click', checkInput);
-var f = document.getElementById('char').addEventListener('keyup', checkKeyPress);
-var g = document.getElementById('howTall').addEventListener('keyup', checkKeyPress);
-
-
+// event listeners
+var e = document.getElementById("button").addEventListener("click", checkInput);
+var f = document.getElementById("char").addEventListener("keyup", checkKeyPress);
+var g = document.getElementById("howTall").addEventListener("keyup", checkKeyPress);
+// verification
 function checkKeyPress(event) {
     if (event.keyCode == "13") {
         checkInput();
     }
 }
 
+// verify input integrity
 function checkInput() {
     var tree = {};
-
-    var check = parseInt(document.getElementById('howTall').value);
+    var check = parseInt(document.getElementById("howTall").value);
     if (isNaN(check)) {
-        alert('Try again. Use a number in the first box.');
+        alert("Try again. Use a number in the first box.");
     } else if (check > 20) {
-        alert('A number between 1 and 20, please');
+        alert("A number between 1 and 20, please");
     } else if (check < 1) {
-        alert('A number between 1 and 20, please');
+        alert("A number between 1 and 20, please");
     } else {
         tree.height = check;
         console.log(tree.height);
     }
 
-
-    var checker = document.getElementById('char').value;
+// verify input integrity
+    var checker = document.getElementById("char").value;
     if (checker.length < 1) {
-        alert('Try again. Fill in the character to use.');
+        alert("Try again. Fill in the character to use.");
     } else if (checker.length > 1) {
-        alert('Just one key entry. Any key');
+        alert("Just one key entry. Any key");
     } else {
         tree.character = checker;
         console.log(tree.character);
     }
     buildTree(tree);
 }
-
+// function to build tree
 function buildTree(tree) {
     var treeHeight = tree.height;
         // console.log(treeHeight)
@@ -51,7 +51,7 @@ function buildTree(tree) {
     var counter = 1;
     for (var i = 0; i < treeHeight; i++) {
 
-        console.log(' '.repeat(treeHeight - i) + treeCharacter.repeat(counter));
+        console.log(" ".repeat(treeHeight - i) + treeCharacter.repeat(counter));
         counter += 2;
     }
 
